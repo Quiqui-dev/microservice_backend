31ca1330450cc2fed4ba7b377d105cce2aa2a433
Kieran Mitchell <kieran@Kierans-MacBook-Pro.local>
2023-01-07 21:04:56 +0000

    adding routes to update employee information

--- python/src/user/server.py
+++ python/src/user/server.py
@@ -77,16 +77,108 @@ def new_employee():
         
         # add new employee
 
-        
-        res = cur.execute(
-            """
-            INSERT INTO employee (first_name, surname, email, standard_rate, hours_worked, overtime_rate, overtime_hours, contract, password)
-            VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s);
-            """,
-            (employee['first_name'], employee['surname'], employee['email'], employee['standard_rate'], 0, employee['overtime_rate'], 0, employee['contract'], employee['password'])
-        )
+        try:
+
+            res = cur.execute(
+                """
+                INSERT INTO employee (first_name, surname, email, standard_rate, hours_worked, overtime_rate, overtime_hours, contract, password)
+                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s);
+                """,
+                (employee['first_name'], employee['surname'], employee['email'], employee['standard_rate'], 0, employee['overtime_rate'], 0, employee['contract'], employee['password'])
+            )
+            mysql.connection.commit()
+            
+            return ("success", 200) 
+        except:
+            mysql.connection.rollback()
+
+            return ("insert failed", 400)
     else:
         return ("permissions invalid for this user", 401)
 
+@server.route("/set_hours_worked", methods=["POST"])
+def set_hours_worked():
+
+    has_access, err = validate.token(request)
+
+    if err:
+        return err
+
+    has_access = json.loads(has_access)
+
+    # check that the employee to update exists and that if they do this user can edit their details.
+    cur = mysql.connection.cursor()
+    
+    res = cur.execute(
+        """SELECT email FROM employee WHERE email=%s""", (request.json["email"])
+    )
+
+    if res:
+        if has_access["admin"] or has_access["username"] == request.json["email"]:
+            # can do the update
+            
+            try:
+                res = cur.execute(
+                    """
+                    UPDATE employee
+                    SET hours_worked=%s,overtime_hours=%s
+                    WHERE email=%s
+                    """,
+                    (request.json["hours_worked"], request.json["overtime_hours"], request.json["email"])
+                )
+            
+                mysql.connection.commit()
+                return ("success", 200)
+            except:
+                mysql.connection.rollback()
+                return ("update failed", 400)
+                
+        else:
+            return ("this user does not have the correct permissions", 401)
+    else:
+        return ("no such employee", 404)
+
+@server.route("/update_employee", methods=["POST"])
+def update_employee():
+
+    has_access, err = validate.token(request)
+
+    if err:
+        return err
+
+    has_access = json.loads(has_access)
+
+    if has_access["admin"]:
+            
+        cur = mysql.connection.cursor()
+
+        res = cur.execute(
+            """SELECT email FROM employee WHERE email=%s""", (request.json["email"])
+        )
+
+        if res:
+            
+            try:
+                res = cur.execute(
+                    """
+                    UPDATE employee
+                    SET standard_rate=%s, overtime_rate=%s, contract=%s
+                    WHERE email=%s 
+                    """,
+                    (request.json["standard_rate"], request.json["overtime_rate"], request.json["contract"], request.json["email"])
+                )
+
+                mysql.connection.commit()
+                return ("success", 200)
+            except:
+                mysql.connection.rollback()
+                return ("update failed", 400)
+        else:
+            return ("no such employee", 404)
+    else:
+        return ("User does not have permissions for this action", 401)
+
+
+
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=5000)
+    server.run(host="0.0.0.0", port=8000)
